2a41a8dfd9e8c3a07ba23ffc51e9a0a16380265a
Dzmitry Rybak <nakadisa@icloud.com>
2025-04-02 16:34:33 +0200

    implemented GET /depth endpoint | added config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,2 @@
 export const PORT = 3000
+export const BINANCE_API_URL = 'https://api.binance.com'
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
-import express from 'express'
-import { PORT } from './config/config'
+import express, { NextFunction, Request, Response } from 'express'
+import { BINANCE_API_URL, PORT } from './config/config'
+import mongoose from 'mongoose'
+import CustomError from './utils/customError'
 
 const app = express()
+
+app.get('/depth', async (req, res, next) => {
+    try {
+        const { symbol } = req.query
+        const response = await fetch(
+            `${BINANCE_API_URL}/api/v3/depth?symbol=${symbol}`
+        )
+
+        if (!response) {
+            throw new CustomError(
+                'Error while fetching data "/api/v3/depth"',
+                404
+            )
+        }
+
+        const data = await response.json()
+
+        res.status(200).json({ data })
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).send({ message: err.message })
+})
+
+mongoose
+    .connect('mongodb://localhost:27017/binanceApiTask')
+    .then(() => console.log('DataBase connected'))
+    .catch((error) => console.error(error.message))
 
 app.listen(PORT, () => {
     console.log(`Server listen on ${PORT}`)
